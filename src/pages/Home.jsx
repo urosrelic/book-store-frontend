a1f516d3148d5769548982391e6fc326a97e6569
urosrelic <urosrelicc@gmail.com>
2024-03-07 20:28:40 +0100

    style: update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,20 @@
 export const Home = () => {
-  return <h1>HOME</h1>;
+  return (
+    <div className='home-container'>
+      <div className='home-about'>
+        <div className='home-about-main-text'>
+          Find Your{' '}
+          <span style={{ textDecoration: 'underline' }}>Favorite</span> Books.
+        </div>
+        <p className='home-about-paragraph'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          vitae rerum enim itaque dolorem ullam veniam dolor.
+        </p>
+        <button>Button</button>
+      </div>
+      <div className='home-image'>
+        <img src='/public/hero_image.png' />
+      </div>
+    </div>
+  );
 };
