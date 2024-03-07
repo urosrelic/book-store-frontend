6c1cbe6df0ed2709e6cb6a032c21859b65b6cc18
urosrelic <urosrelicc@gmail.com>
2024-03-07 21:28:02 +0100

    feat: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,50 @@
 export const Footer = () => {
-  return <div className='footer'>FOOTER</div>;
+  return (
+    <div className='footer'>
+      <div className='footer-upper'>
+        <div className='footer-site-logo'>
+          <img src='/public/logo.png' />
+        </div>
+        <div className='footer-media-links'>
+          <img src='/public/facebook-svgrepo-com.svg' />
+          <img src='/public/twitter-svgrepo-com.svg' />
+          <img src='/public/linkedin-1-svgrepo-com.svg' />
+        </div>
+      </div>
+      <div className='footer-lower'>
+        <div className='footer-lower-container'>
+          <h3>Company</h3>
+          <div>
+            <div className='footer-item'>Item</div>
+            <div className='footer-item'>Item</div>
+            <div className='footer-item'>Item</div>
+          </div>
+        </div>
+        <div className='footer-lower-container'>
+          <h3>Social</h3>
+          <div>
+            <div className='footer-item'>Item</div>
+            <div className='footer-item'>Item</div>
+            <div className='footer-item'>Item</div>
+          </div>
+        </div>
+        <div className='footer-lower-container'>
+          <h3>Social</h3>
+          <div>
+            <div className='footer-item'>Item</div>
+            <div className='footer-item'>Item</div>
+            <div className='footer-item'>Item</div>
+          </div>
+        </div>
+        <div className='footer-lower-container'>
+          <h3>Social</h3>
+          <div>
+            <div className='footer-item'>Item</div>
+            <div className='footer-item'>Item</div>
+            <div className='footer-item'>Item</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
