525a8d8c4011bd078f1c2c5aff028624c743bb78
Uroš Relić <urosrelicc@gmail.com>
2024-03-09 18:33:07 +0100

    feat: add vite proxy for backend

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8080',
-  //       changeOrigin: true,
-  //       secure: false,
-  //       rewrite: (path) => path.replace('/api', ''),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace('/api', ''),
+      },
+    },
+  },
 });
