5d6c9f7838f42ffa6555729512b5113b19e664f6
urosrelic <urosrelicc@gmail.com>
2024-03-11 21:30:27 +0100

    feat: update login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,54 +1,85 @@
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
+import { useState } from 'react';
 import axios from 'axios';
+import { InputField } from '../components/InputField/InputField';
 
 export const Login = () => {
-  const schema = Yup.object().shape({
-    username: Yup.string().required('Username is required'),
-    password: Yup.string().required('Password is required'),
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
   });
+  const [errors, setErrors] = useState({});
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
+  const handleInputChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-  const onSubmit = async (data) => {
-    console.log(data);
-    try {
-      const response = await axios.get('/api/users/login', {
-        params: { username: data.username, password: data.password },
-      });
-      if (response.status === 200) {
-        console.log('User logged in successfully');
-      } else {
-        console.error('Incorrect username or password');
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!formData.username.trim()) {
+      newErrors.username = 'Username is required';
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = 'Password is required';
+    }
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0; // Return true if there are no errors
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    if (validateForm()) {
+      try {
+        const response = await axios.get('/api/users/login', {
+          params: {
+            username: formData.username,
+            password: formData.password,
+          },
+        });
+
+        if (response.status === 200) {
+          console.log('User logged in successfully');
+          // Optionally, you can redirect the user to another page or perform other actions
+        } else {
+          console.error('Incorrect username or password');
+        }
+      } catch (error) {
+        console.error('Error: ', error);
       }
-    } catch (error) {
-      console.error('Error: ', error);
     }
   };
 
   return (
     <div className='login-container'>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='form-control'>
-          <label>Username:</label>
-          <input type='text' name='username' {...register('username')} />
-          {errors.username && (
-            <div className='error-message'>{errors.username.message}</div>
-          )}
-        </div>
-        <div className='form-control'>
-          <label>Password:</label>
-          <input type='password' name='password' {...register('password')} />
-          {errors.password && (
-            <div className='error-message'>{errors.password.message}</div>
-          )}
-        </div>
+      <form onSubmit={onSubmit}>
+        <InputField
+          id='username-input'
+          labelName='Username'
+          name='username'
+          type='text'
+          value={formData.username}
+          onChange={handleInputChange}
+          errorMessage={errors.username}
+        />
+        <InputField
+          id='password-input'
+          labelName='Password'
+          name='password'
+          type='password'
+          value={formData.password}
+          onChange={handleInputChange}
+          errorMessage={errors.password}
+        />
+
         <button type='submit'>Login</button>
       </form>
     </div>
