f9c2da1f1929763be8728954145d27bba0620818
urosrelic <urosrelicc@gmail.com>
2024-03-14 11:29:03 +0100

    feat: add animation on clear text button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,19 @@
 import { FaSearch } from 'react-icons/fa';
 import { Button } from './Button/Button.styled';
+import { MdCancel } from 'react-icons/md';
+import { useState } from 'react';
 
 export const Search = () => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const clearText = () => {
+    setQuery('');
+  };
+
   const buttonStyles = {
     width: '5rem',
     height: '3rem',
@@ -11,12 +23,16 @@ export const Search = () => {
     hoverTextColor: '#17242a',
   };
 
+  const cancelButtonClass =
+    query.length > 0 ? 'clear-text-btn active' : 'clear-text-btn';
+
   return (
     <div className='search-container'>
       <div className='search-container-items'>
         <div className='search-box'>
           <FaSearch className='search-icon' />
-          <input type='text' />
+          <input type='text' value={query} onChange={handleInputChange} />
+          <MdCancel className={cancelButtonClass} onClick={clearText} />
         </div>
         <Button className='search-button' styles={buttonStyles}>
           Search
