c39f285b4fd64221b9f59cf8485145fe38c45627
urosrelic <urosrelicc@gmail.com>
2024-03-16 15:53:57 +0100

    feat: add loading state to private routes
    This fixes an error where if the loading state is not provided, user gets redirected to the home screen even if he is logged in

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,6 +1,11 @@
-import { Outlet, Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 export const PrivateRoutes = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return isAuthenticated ? <Outlet /> : <Navigate to='/login' />;
 };
