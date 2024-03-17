6f455ed128c86aea3986d06710d0c6830a695eb2
urosrelic <urosrelicc@gmail.com>
2024-03-17 14:21:39 +0100

    feat: update styling of Dashboard and UserDetails

--- src/components/User/UserDetails.jsx
+++ src/components/User/UserDetails.jsx
@@ -1,12 +1,15 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useAuth } from '../../hooks/useAuth';
 export const UserDetails = () => {
   const { currentUser } = useAuth();
   return (
     <div className='user-details'>
-      <h1>
-        Welcome back{' '}
-        <span className='dashboard-user'>{currentUser.username}</span>{' '}
-      </h1>
+      <div className='user-avatar'>
+        <AccountCircleIcon id='account-icon' />
+      </div>
+      <div className='user-information'>
+        <h2>Welcome back {currentUser.username}</h2>
+      </div>
     </div>
   );
 };
