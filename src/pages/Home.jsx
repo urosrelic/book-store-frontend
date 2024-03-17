b96cd34367721d1af5be9a2cde92e9b8234504e1
urosrelic <urosrelicc@gmail.com>
2024-03-17 18:24:24 +0100

    feat: update layout at 1000px width media query

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,60 @@
+import { useEffect, useState } from 'react';
+
 export const Home = () => {
-  return (
-    <div className='home-container'>
-      <div className='home-main-text'>
-        Find Your <span style={{ textDecoration: 'underline' }}>Favorite</span>{' '}
-        Books.
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 1000);
+
+  const smallScreenLayout = () => {
+    return (
+      <div className='home-container'>
+        <div className='home-main-text'>
+          Find Your{' '}
+          <span style={{ textDecoration: 'underline' }}>Favorite</span> Books.
+        </div>
+        <div className='home-image'>
+          <img src='/hero_image.png' />
+        </div>
+        <p className='home-paragraph'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          vitae rerum enim itaque dolorem ullam veniam dolor.
+        </p>
+        <button id='get-started-btn'>Get Started</button>
       </div>
-      <div className='home-image'>
-        <img src='/hero_image.png' />
+    );
+  };
+
+  const wideScreenLayout = () => {
+    return (
+      <div className='home-container'>
+        <div className='home-content'>
+          <div className='home-main-text'>
+            Find Your{' '}
+            <span style={{ textDecoration: 'underline' }}>Favorite</span> Books.
+          </div>
+          <p className='home-paragraph'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            vitae rerum enim itaque dolorem ullam veniam dolor.
+          </p>
+          <button id='get-started-btn'>Get Started</button>
+        </div>
+
+        <div className='home-image'>
+          <img src='/hero_image.png' />
+        </div>
       </div>
-      <p className='home-paragraph'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        vitae rerum enim itaque dolorem ullam veniam dolor.
-      </p>
-      <button id='get-started-btn'>Get Started</button>
-    </div>
-  );
+    );
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth >= 1000);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return isWideScreen ? wideScreenLayout() : smallScreenLayout();
 };
