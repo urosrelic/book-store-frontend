b9cf9dd62e59b4ec208445f1c06275907472402a
urosrelic <urosrelicc@gmail.com>
2024-03-18 16:20:20 +0100

    feat: update home layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,58 +1,20 @@
-import { useEffect, useState } from 'react';
 import './Home.css';
 export const Home = () => {
-  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 1000);
-
-  const smallScreenLayout = () => {
-    return (
-      <div className='home-container'>
-        <div className='home-main-text'>
-          Find Your <span>Favorite</span> Books.
-        </div>
-        <div className='home-image'>
-          <img src='/hero_image.png' />
-        </div>
-        <p className='home-paragraph'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          vitae rerum enim itaque dolorem ullam veniam dolor.
-        </p>
-        <button id='get-started-btn'>Get Started</button>
+  return (
+    <div className='home-container'>
+      <div className='home-main-text'>Find Your Favorite Books</div>
+      <div className='home-main-logo'>
+        book<span>store</span>
       </div>
-    );
-  };
-
-  const wideScreenLayout = () => {
-    return (
-      <div className='home-container'>
-        <div className='home-content'>
-          <div className='home-main-text'>
-            Find Your <span>Favorite</span> Books.
-          </div>
-          <p className='home-paragraph'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            vitae rerum enim itaque dolorem ullam veniam dolor.
-          </p>
-          <button id='get-started-btn'>Get Started</button>
+      <div className='home-paragraph'>
+        <div className='home-paragraph-quote'>
+          “Life is a learning experience, only if you learn.”
         </div>
-
-        <div className='home-image'>
-          <img src='/hero_image.png' />
+        <div className='home-paragraph-quote-author'>
+          <span>Yogi Berra</span>
         </div>
       </div>
-    );
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsWideScreen(window.innerWidth >= 1000);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  return isWideScreen ? wideScreenLayout() : smallScreenLayout();
+      <button id='get-started-btn'>Get Started</button>
+    </div>
+  );
 };
