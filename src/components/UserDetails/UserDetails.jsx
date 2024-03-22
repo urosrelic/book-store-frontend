172f0f43e673e7f7c7f878df171dd3d9d161f936
urosrelic <urosrelicc@gmail.com>
2024-03-22 17:07:12 +0100

    feat: show purchase history in dashboard

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,17 +1,22 @@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useAuth } from '../../hooks/useAuth';
 
+import { Purchases } from '../Purchases/Purchases';
 import './UserDetails.css';
 
 export const UserDetails = () => {
   const { currentUser } = useAuth();
+
+  // console.log(purchase);
+
   return (
     <div className='user-details'>
-      <div className='user-avatar'>
-        <AccountCircleIcon id='account-icon' />
-      </div>
       <div className='user-information'>
+        <AccountCircleIcon id='account-icon' />
         <h2>Welcome back {currentUser.username}</h2>
+      </div>
+      <div className='user-purchases'>
+        <Purchases userId={currentUser.userId} />
       </div>
     </div>
   );
