d20933111af6231cd45b8671535f3d730733cb56
urosrelicc <urosrelicc@gmail.com>
2025-06-24 14:59:28 +0200

    try vercel config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,22 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  // eslint-disable no-undef
+
+  const env = loadEnv(mode, process.cwd(), '');
+  // eslint-disable no-undef
+  const backendUrl = process.env.VERCEL_URL
+    // eslint-disable no-undef
+
+    ? `https://${process.env.VERCEL_URL}`
+    : env.VITE_BOOK_STORE_BACKEND_HOST;
 
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_BOOK_STORE_BACKEND_HOST,
+          target: backendUrl,
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace('/api', '')
