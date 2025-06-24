b016926edd866c0374a26b716ea23aca6d960e90
urosrelicc <urosrelicc@gmail.com>
2025-06-24 15:31:56 +0200

    update API proxy configuration to use regex for path matching

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 
+/* eslint-env node */
 export default defineConfig(({ mode }) => {
-  // eslint-disable-next-line no-undef
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
     server: {
       proxy: {
-        '/api': {
+        '^/api/.*': {
           target: env.VITE_BOOK_STORE_BACKEND_HOST,
           changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace('/api', '')
+          secure: false
         }
       }
     }
